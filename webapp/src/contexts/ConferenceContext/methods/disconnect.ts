da9eb74b015574b001489549bbf0b55051ec02f1
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-03-19 16:09:32 +0100

    fix: stop local stream tracks when disconnected

--- webapp/src/contexts/ConferenceContext/methods/disconnect.ts
+++ webapp/src/contexts/ConferenceContext/methods/disconnect.ts
@@ -2,8 +2,11 @@ import { ConferenceActionType, type ConferenceAction } from '../ConferenceAction
 import { type ConferenceState } from '../ConferenceState'
 
 export const disconnect = async (state: ConferenceState, dispatch: React.Dispatch<ConferenceAction>): Promise<void> => {
-  await state.client?.disconnect({ reason: 'User initiated disconnect' })
   dispatch({
     type: ConferenceActionType.Disconnected
   })
+
+  await state.client?.disconnect({ reason: 'User initiated disconnect' })
+
+  state.localStream?.getTracks().forEach((track) => { track.stop() })
 }
