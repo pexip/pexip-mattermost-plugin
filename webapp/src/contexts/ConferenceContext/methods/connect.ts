2fcb30469bee999fa8770ab155734499919104e7
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-04-22 09:56:35 +0200

    style(connect): remove commented code

--- webapp/src/contexts/ConferenceContext/methods/connect.ts
+++ webapp/src/contexts/ConferenceContext/methods/connect.ts
@@ -78,61 +78,6 @@ export const connect = async (params: ConnectParams, dispatch: React.Dispatch<Co
   }
 
   if (response?.status === 200) {
-    // setUserSettingsEventListener((userSettings) => {
-    //   const { audioMuted } = state
-    //   console.error(audioMuted)
-    // })
-    // setUserSettingsListener((userSettings) => {
-    //   // TODO: Only stop the local stream that changed or that aren\'t muted
-
-    //   // TODO: Get the localStream
-    //   const audioTracks = localStream.getAudioTracks()
-    //   const audioMuted = audioTracks.length === 0 || !audioTracks.some((track) => track.readyState === 'live')
-
-    //   const videoTracks = localStream.getVideoTracks()
-    //   const videoMuted = videoTracks.length === 0 || !videoTracks.some((track) => track.readyState === 'live')
-
-    //   console.error(audioTracks.length)
-    //   console.error(audioTracks[0].readyState)
-    //   console.error(videoTracks.length)
-    //   console.error(videoTracks[0].readyState)
-    //   // TODO: Only stop the local stream that changed or that aren't muted
-    //   // track.getCapabilities().deviceId
-    //   console.error(audioMuted, videoMuted)
-
-    //   if (!(audioMuted && videoMuted)) {
-    //     // console.error({
-    //     //   audio: audioMuted ? false : { deviceId: userSettings.inputAudioDeviceId },
-    //     //   video: videoMuted ? false : { deviceId: userSettings.inputVideoDeviceId }
-    //     // })
-    //     localStream.getTracks().forEach((track) => { track.stop() })
-
-    //     navigator.mediaDevices.getUserMedia({
-    //       audio: audioMuted ? false : { deviceId: userSettings.inputAudioDeviceId },
-    //       video: videoMuted ? false : { deviceId: userSettings.inputVideoDeviceId }
-    //     })
-    //       .then((localStream) => {
-    //         client.setStream(localStream)
-    //         dispatch({
-    //           type: ConferenceActionType.ChangeDevices,
-    //           body: {
-    //             localStream,
-    //             audioSinkId: userSettings.outputAudioDeviceId
-    //           }
-    //         })
-    //       })
-    //       .catch((e) => { console.error(e) })
-    // }
-    // TODO: Change sinkId
-    // dispatch({
-    //   type: ConferenceActionType.Connected,
-    //   body: {
-    //     client,
-    //     localStream,
-    //     audioSinkId: userSettings.outputAudioDeviceId
-    //   }
-    // })
-    // })
     dispatch({
       type: ConferenceActionType.Connected,
       body: {
