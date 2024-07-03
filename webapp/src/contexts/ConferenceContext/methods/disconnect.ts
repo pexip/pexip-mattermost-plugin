81ad8847098a94ec92bf6977f6d25a6f7d93ba08
Marcos Cereijo <marcos.rodriguez@pexip.com>
2024-07-03 13:46:41 +0200

    fix(disconnect): only notify to mattermost if the state is connected

--- webapp/src/contexts/ConferenceContext/methods/disconnect.ts
+++ webapp/src/contexts/ConferenceContext/methods/disconnect.ts
@@ -3,17 +3,20 @@ import { ConferenceActionType, type ConferenceAction } from '../ConferenceAction
 import { type ConferenceState } from '../ConferenceState'
 import { getMattermostStore } from 'src/utils/mattermost-store'
 import { notifyLeaveConference } from 'src/utils/http-requests'
+import { ConnectionState } from 'src/types/ConnectionState'
 
 export const disconnect = async (
   state: ConferenceState,
   dispatch: React.Dispatch<ConferenceAction>,
   reason: DisconnectReason
 ): Promise<void> => {
-  state.client?.disconnect({ reason }).catch(console.error)
-  
-  notifyLeaveConference(getMattermostStore().getState().entities.channels.currentChannelId).catch((error) => {
-    console.error(error)
-  })
+  if (state.connectionState === ConnectionState.Connected) {
+    state.client?.disconnect({ reason }).catch(console.error)
+
+    notifyLeaveConference(getMattermostStore().getState().entities.channels.currentChannelId).catch((error) => {
+      console.error(error)
+    })
+  }
 
   dispatch({
     type: ConferenceActionType.Disconnected,
